a9b63e935df49208c0f261cad76e1757b184ca39
AMSIDCON <amsidcon.srm.ccp@gmail.com>
2025-07-17 12:08:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,10 +30,7 @@ form.addEventListener('submit', async (e) => {
     method: 'POST',
     body: formData
   });
-
   const text = await response.text();
-  console.log("Google Apps Script response:", text); // ✅ LOG FOR DEBUGGING
-
   if (!text.includes("Registration successful")) {
     alert("⚠️ You have already attempted the quiz with this Email or Phone number.");
     return;
